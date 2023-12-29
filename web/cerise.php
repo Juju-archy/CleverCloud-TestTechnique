c2f13301ed0e7fcfa2aa449d5acc8657313c69b7
Julie <63869305+Juju-archy@users.noreply.github.com>
2023-12-29 23:17:09 +0100

    amélioration exo 3

--- web/cerise.php
+++ web/cerise.php
@@ -1,6 +1,18 @@
 <?php 
 
-if(!empty(file_get_contents("./bigF")))
-{ 
-    echo 'File loaded!'; 
+$filePath = "./bigF";
+
+// Vérifier si le fichier existe
+if (file_exists($filePath)) {
+    // Lire le contenu du fichier
+    $fileContent = file_get_contents($filePath);
+
+    // Vérifier si le fichier n'est pas vide
+    if (!empty($fileContent)) {
+        echo 'File loaded!';
+    } else {
+        echo 'File exists, but it is empty.';
+    }
+} else {
+    echo 'File does not exist.';
 }
